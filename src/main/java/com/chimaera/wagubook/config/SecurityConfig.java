cc4bf5af44ded53479652c11da7b0921ce13d255
jayjae97 <skyroof97@gmail.com>
2024-07-02 11:02:26 +0900

    [FIX]/#1-로그인_포스트 기본 기능 구현

--- src/main/java/com/chimaera/wagubook/config/SecurityConfig.java
+++ src/main/java/com/chimaera/wagubook/config/SecurityConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,16 +17,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth -> {
+//            auth
+//                    .requestMatchers("/", "/login", "/join", "/logout", "/swagger-ui/**","/v3/api-docs/**")
+//                    .permitAll()
+//                    .anyRequest()
+//                    .authenticated();
+//        });
+//
+//
+//        http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> {
-            auth
-                    .requestMatchers("/", "/login", "/join", "/logout", "/swagger-ui/**", "/v3/api-docs/**")
-                    .permitAll()
-                    .anyRequest()
-                    .authenticated();
-        });
-
-
-        http.csrf(csrf -> csrf.disable());
+                    auth
+                            .requestMatchers("/**", "/login", "/join", "/logout", "/swagger-ui/**", "/v3/api-docs/**", "/error", "/h2-console/**")
+                            .permitAll()
+                            .anyRequest()
+                            .authenticated();
+                })
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/**")))
+                .headers(headers -> {
+                    headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+                })
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
