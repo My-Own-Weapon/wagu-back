78374fba44509ba26269a59a264c962b73af9032
HyeokJoon <fufu98@naver.com>
2024-07-04 21:44:54 +0900

    [FEAT]#15-share 랜덤 url을 share_id로 변환

--- src/main/java/com/chimaera/wagubook/controller/ShareController.java
+++ src/main/java/com/chimaera/wagubook/controller/ShareController.java
@@ -8,6 +8,8 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShareController {
     private final ShareService shareService;
 
+    /**
+     * 랜덤 url 생성
+     * Method : POST
+     * url : members/share
+     * */
     @PostMapping("/share")
     public ResponseEntity<ShareResponse> createUrl(HttpSession session){
 
@@ -24,6 +31,28 @@ public class ShareController {
             throw new CustomException(ErrorCode.REQUEST_LOGIN);
         }
 
-        return new ResponseEntity<>(shareService.createUrl(memberId), HttpStatus.OK);
+        return new ResponseEntity<>(shareService.createUrl(), HttpStatus.OK);
     }
+
+    /**
+     * 랜덤 url로 접근 했을 때 share_id를 반환 ++++ 사용할 수 없으면 null 반환
+     * Method : GET
+     * url : /share/{random_url}
+     * ex : /share/wv1Vpc4Eqt
+     */
+    @GetMapping("/share/{url}")
+    public ResponseEntity<String> findShareId(@PathVariable String url, HttpSession session){
+
+        Long memberId = (Long) session.getAttribute("memberId");
+        if (memberId == null) {
+            throw new CustomException(ErrorCode.REQUEST_LOGIN);
+        }
+
+        return new ResponseEntity<>(shareService.findShareId(url), HttpStatus.OK);
+    }
+
+
+//    @GetMapping("/share/{share_id}/map")
+
+
 }
--- src/main/java/com/chimaera/wagubook/service/ShareService.java
+++ src/main/java/com/chimaera/wagubook/service/ShareService.java
@@ -5,10 +5,15 @@ import com.chimaera.wagubook.entity.Share;
 import com.chimaera.wagubook.repository.ShareRepository;
 import com.chimaera.wagubook.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.Optional;
 import java.util.Random;
 
@@ -17,14 +22,8 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class ShareService {
     private final ShareRepository shareRepository;
-    private final MemberRepository memberRepository;
 
-    /**
-     *
-     *
-     * */
-    public ShareResponse createUrl(Long memberId) {
-
+    public ShareResponse createUrl() {
 
         //랜덤 10자리 숫자 생성
         boolean loop = true;
@@ -57,5 +56,21 @@ public class ShareService {
                 .limit(limit)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
+    }
+
+    public String findShareId(String url) {
+        Optional<Share> os = shareRepository.findByUrl(url);
+        if(os.isPresent()){
+            Share findShare = os.get();
+            //유효기간(30분) 만료 전일 때만 반환
+            Duration duration = Duration.between(findShare.getLocalDateTime(),LocalDateTime.now());
+            System.out.println("localDateTime : " + LocalDateTime.now());
+            System.out.println("ShareDAteTime : " + findShare.getLocalDateTime());
+            System.out.println("duration : "+duration.getSeconds());
+            if(duration.getSeconds() < 1800){
+                return "" + os.get().getId();
+            }
+        }
+        return "해당 url이 존재하지 않습니다.";
     }
 }
