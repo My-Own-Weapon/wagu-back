6e0250e7fce7e51c3f848271814ce9c1dbb43084
수현 <su551732@gmail.com>
2024-07-05 16:10:06 +0900

    [FEAT]/#2-S3에 이미지 업로드, 삭제 구현

--- src/main/java/com/chimaera/wagubook/exception/ErrorCode.java
+++ src/main/java/com/chimaera/wagubook/exception/ErrorCode.java
@@ -14,6 +14,14 @@ public enum ErrorCode {
     UNABLE_TO_DELETE_POST(BAD_REQUEST, "포스트를 삭제할 수 없습니다."),
     NOT_ALLOW_FOLLOW(BAD_REQUEST, "허용되지 않는 팔로우입니다."),
     WRONG_PASSWORD_CONFIRM(BAD_REQUEST, "비밀번호를 다시 확인해주세요."),
+
+    EMPTY_FILE(BAD_REQUEST, "요청된 파일이 없습니다."),
+    NO_FILE_EXTENSION(BAD_REQUEST, "확장자가 없는 파일입니다."),
+    INVALID_FILE_EXTENSION(BAD_REQUEST, "허용되지 않는 확장자입니다."),
+    FAIL_TO_UPLOAD_IMAGE(BAD_REQUEST, "이미지 업로드에 실패했습니다."),
+    IO_EXCEPTION_ON_IMAGE_UPLOAD(BAD_REQUEST, "파일에 입출력 문제가 발생하였습니다."),
+    IO_EXCEPTION_ON_IMAGE_DELETE(BAD_REQUEST, "파일에 입출력 문제가 발생하였습니다."),
+
     
     // 401 UNAUTHORIZED: 인증되지 않은 사용자
     REQUEST_LOGIN(UNAUTHORIZED, "로그인이 필요합니다."),
