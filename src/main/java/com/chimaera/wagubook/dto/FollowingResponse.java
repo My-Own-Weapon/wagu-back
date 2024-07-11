4225d2bd88f6d7d04251dbecc5909adaeaff615c
HyeokJoon <fufu98@naver.com>
2024-07-11 21:19:51 +0900

    [FEAT]/#46-follow response추가

--- src/main/java/com/chimaera/wagubook/dto/FollowingResponse.java
+++ src/main/java/com/chimaera/wagubook/dto/FollowingResponse.java
@@ -1,6 +1,7 @@
 package com.chimaera.wagubook.dto;
 
 import com.chimaera.wagubook.entity.Follow;
+import com.chimaera.wagubook.entity.Member;
 import lombok.Data;
 
 @Data
@@ -8,10 +9,17 @@ public class FollowingResponse {
     private Long memberId;
     private String username;
     private boolean isEach;
+    private String memberImageUrl;
+    private boolean isOnLive;
 
     public FollowingResponse(Follow follow) {
-        this.memberId = follow.getFromMember().getId();
-        this.username = follow.getFromMember().getUsername();
+        Member member = follow.getFromMember();
+        String u = member.getMemberImage().getUrl();
+
+        this.memberId = member.getId();
+        this.username = member.getUsername();
+        this.memberImageUrl = (u==null) ? null : u;
         this.isEach = follow.isEach();
+        this.isOnLive = member.isOnLive();
     }
 }
