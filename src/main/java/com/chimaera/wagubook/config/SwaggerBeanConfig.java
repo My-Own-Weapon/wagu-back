a7c75412f0f70859364236d0a58caf2616e0fa97
수현 <su551732@gmail.com>
2024-07-12 21:02:16 +0900

    [FEAT]/#38-Swagger CoConfig 추가(form-data, json 동시 테스트)

---
+++ src/main/java/com/chimaera/wagubook/config/SwaggerBeanConfig.java
@@ -0,0 +1,16 @@
+package com.chimaera.wagubook.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerBeanConfig {
+    public SwaggerBeanConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+}
