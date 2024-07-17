b0a2814865ceebc59532fc62f7bbd008b7f5aad4
jayjae97 <skyroof97@gmail.com>
2024-07-17 22:59:53 +0900

    [FIX]/#64- pagenation 수정

--- src/main/java/com/chimaera/wagubook/dto/request/MemberRequest.java
+++ src/main/java/com/chimaera/wagubook/dto/request/MemberRequest.java
@@ -1,12 +1,18 @@
 package com.chimaera.wagubook.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class MemberRequest {
+    @NotNull
     private String username;
+    @NotNull
     private String password;
+    @NotNull
     private String passwordConfirm;
+    @NotNull(message = "이름은 한글이어야 합니다.")
     private String name;
+    @NotNull(message = "전화번호는 -가 포함된 ")
     private String phoneNumber;
 }
