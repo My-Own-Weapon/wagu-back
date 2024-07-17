1c1f478688fa777ca3a45dd65de275af77725c10
수현 <su551732@gmail.com>
2024-07-17 20:07:35 +0900

    [FIX]/#38-Post 관련 조회에 페이징 추가, Permission 고려 수정

--- src/main/java/com/chimaera/wagubook/repository/post/PostRepository.java
+++ src/main/java/com/chimaera/wagubook/repository/post/PostRepository.java
@@ -1,6 +1,8 @@
 package com.chimaera.wagubook.repository.post;
 
 import com.chimaera.wagubook.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom{
     Optional<Post> findByIdAndMemberId(Long postId, Long memberId);
-    List<Post> findAllByMemberId(Long memberId);
+    Page<Post> findAllByMemberId(Long memberId, PageRequest pageRequest);
     List<Post> findAllByStoreId(Long storeId);
     int countByMemberId(Long memberId);
     List<Post> searchPostsByMemberIdAndStoreName(Long memberId, String keyword);
