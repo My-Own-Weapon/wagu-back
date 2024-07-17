03f58e61cc06c75a69f560643fbcc63c9b95d78d
JAEHAK KIM <skyroof97@gmail.com>
2024-07-17 23:04:33 +0900

    Update MemberRequest.java

--- src/main/java/com/chimaera/wagubook/dto/request/MemberRequest.java
+++ src/main/java/com/chimaera/wagubook/dto/request/MemberRequest.java
@@ -13,6 +13,6 @@ public class MemberRequest {
     private String passwordConfirm;
     @NotNull(message = "이름은 한글이어야 합니다.")
     private String name;
-    @NotNull(message = "전화번호는 -가 포함된 ")
+    @NotNull(message = "전화번호는 -가 포함되어야 합니다.")
     private String phoneNumber;
 }
