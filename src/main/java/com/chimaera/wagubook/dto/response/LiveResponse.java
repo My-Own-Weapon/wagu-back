a67adbce3dfb351fd382ff59e23cc0583f0a5317
HyeokJoon <fufu98@naver.com>
2024-07-19 19:10:00 +0900

    [FEAT]#95-storeId로 라이브중인 사람 조회 구현

---
+++ src/main/java/com/chimaera/wagubook/dto/response/LiveResponse.java
@@ -0,0 +1,17 @@
+package com.chimaera.wagubook.dto.response;
+
+import com.chimaera.wagubook.entity.LiveRoom;
+import lombok.Data;
+
+@Data
+public class LiveResponse {
+    String profileImage;    //라이브 중인 사람의 프로필 사진
+    String sessionId;       //라이브 세션 ID
+    String userName;        //라이브 중인 사람의 닉네임
+
+    public LiveResponse(LiveRoom liveRoom){
+        this.profileImage=liveRoom.getMember().getMemberImage().getUrl();
+        this.sessionId=liveRoom.getSessionId();
+        this.userName=liveRoom.getMember().getName();
+    }
+}
