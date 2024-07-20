1b00de9e7438da9711b5f4100a14381bba8ab0c4
HyeokJoon <fufu98@naver.com>
2024-07-20 16:22:18 +0900

    [FEAT]/#15- Redis Lock을 사용한 동시성 문제 해결

--- src/main/java/com/chimaera/wagubook/service/ShareService.java
+++ src/main/java/com/chimaera/wagubook/service/ShareService.java
@@ -66,26 +66,10 @@ public class ShareService {
     }
 
     public String findShareId(String url, Long memberId) {
-//        Optional<Share> os = shareRepository.findByUrl(url);
-////        if(os.isPresent()){
-////            Share findShare = os.get();
-////            //유효기간(30분) 만료 전일 때만 반환
-////            Duration duration = Duration.between(findShare.getLocalDateTime(),LocalDateTime.now());
-////            System.out.println("localDateTime : " + LocalDateTime.now());
-////            System.out.println("ShareDAteTime : " + findShare.getLocalDateTime());
-////            System.out.println("duration : "+duration.getSeconds());
-////            if(duration.getSeconds() < 1800){
-////
-////                return "" + findShare.getId();
-////            }
-////        }
         Share value = (Share) redisRepository.getObject(url);
         if(value == null)
             throw new CustomException(ErrorCode.NOT_FOUND_URL);
-        else
-            return ""+value.getId();
-
-//        return "해당 url이 존재하지 않습니다.";
+        return ""+value.getId();
     }
 
     @Transactional
@@ -100,34 +84,33 @@ public class ShareService {
 
         //공유방에서 리스트 찾기
         Share share = (Share) redisRepository.getObject(url);
-        if(share == null)
+        if(share == null){
             throw new CustomException(ErrorCode.NOT_FOUND_URL);
-//        Optional<Share> osh = shareRepository.findById(Long.parseLong(shareId));
-//        if(osh.isEmpty()){
-//            throw new CustomException(ErrorCode.NOT_FOUND_SHARE);
-//        }
-//        Share share = osh.get();
+        }
         HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
 
+        //TODO: 디버그 코드 추후 삭제
         System.out.println("[before add]");
         for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
             System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
         }
-
-        //이미 포함하고 있으면
         System.out.println(findStore.getId());
-        if(voteStoreList.containsKey(findStore.getId()))
+
+        //이미 포함하고 있으면 ALREADY_ADD
+        if(voteStoreList.containsKey(findStore.getId())){
             throw new CustomException(ErrorCode.ALREADY_ADD);
+        }
 
-        //최대 개수를 넘은 경우
-        if(voteStoreList.size() == 10)
+        //최대 개수를 넘은 경우 OVER_MAX
+        if(voteStoreList.size() == 10){
             throw new CustomException(ErrorCode.OVER_MAX);
+        }
 
+        //변경 후 저장
         voteStoreList.put(findStore.getId(), 0);
-        //변경사항 저장
-//        shareRepository.save(share);
         redisRepository.setValuesObject(url, share, Duration.ofMinutes(5));
 
+        //TODO: 디버그 코드 추후 삭제
         System.out.println("[after add]");
         for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
             System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
@@ -137,23 +120,24 @@ public class ShareService {
 
     @Transactional
     public String deleteVoteStore(String url, String storeId) {
-        //가게 찾기
-        Store findStore = storeRepository.findById(Long.parseLong(storeId)).get();
-        //공유 엔티티에서 제거
-        Share share = (Share) redisRepository.getObject(url);
-        if(share == null)
-            throw new CustomException(ErrorCode.NOT_FOUND_URL);
-//        Share share = shareRepository.findById(Long.parseLong(shareId)).get();
 
+        //공유 엔티티 조회
+        Share share = (Share) redisRepository.getObject(url);
+        if(share == null){
+            throw new CustomException(ErrorCode.NOT_FOUND_URL);
+        }
         HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
 
+
         //투표 리스트에 있으면 제거
-        if(voteStoreList.remove(findStore.getId())!=null){
+        if(voteStoreList.remove(storeId)!=null){
+            //TODO: 디버그 코드 추후 삭제
             System.out.println("[after delete]");
             for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
                 System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
             }
-//            shareRepository.save(share);
+
+            //redis에 저장
             redisRepository.setValuesObject(url, share, Duration.ofMinutes(5));
             return "투표에서 삭제되었습니다.";
         }
@@ -161,50 +145,69 @@ public class ShareService {
     }
 
     public String like(String url, String storeId) throws InterruptedException{
-        System.out.println(redisLockRepository.lock("lock"+url));
-        while (Boolean.FALSE.equals(redisLockRepository.lock("lock"+url))){
+
+        //redis Lock
+        while (Boolean.FALSE.equals(redisLockRepository.lock("like"+url+storeId))){
+            Thread.sleep(10);
+        }
+
+        //redis에서 공유 데이터 조회
+        Share share = (Share) redisRepository.getObject(url);
+        if(share == null){
+            redisLockRepository.unlock(url+storeId);
+            throw new CustomException(ErrorCode.NOT_FOUND_URL);
+        }
+
+        //voteStoreList 데이터 변경
+        HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
+        Long key = Long.parseLong(storeId);
+        voteStoreList.replace(key, voteStoreList.get(key)+1);
+
+        //TODO: 디버그 코드 추후 삭제
+        System.out.println("value : " + voteStoreList.get(key));
+        System.out.println("[after like]");
+        for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
+            System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
+        }
+
+        //redis에 저장
+        redisRepository.setValuesObject(url, share, Duration.ofMinutes(5));
+
+        //redis unLock
+        redisLockRepository.unlock(url+storeId);
+        return "투표 성공";
+    }
+
+    public String likeCancel(String url, String storeId) throws InterruptedException{
+
+        //redis Lock
+        while (Boolean.FALSE.equals(redisLockRepository.lock("likeCancel"+url+storeId))){
             Thread.sleep(10);
         }
 
         //공유 데이터 찾기
         Share share = (Share) redisRepository.getObject(url);
-        if(share == null)
+        if(share == null){
+            redisLockRepository.unlock("likeCancel"+url+storeId);
             throw new CustomException(ErrorCode.NOT_FOUND_URL);
-
-//        Share share = shareRepository.findById(Long.parseLong(shareId)).get();
-
-        HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
-        Long key = Long.parseLong(storeId);
-        voteStoreList.replace(key, voteStoreList.get(key)+1);
-        System.out.println("value : " + voteStoreList.get(key));
-
-        System.out.println("[after like]");
-        for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
-            System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
         }
-//        shareRepository.save(share);
-        redisRepository.setValuesObject(url, share, Duration.ofMinutes(5));
-        redisLockRepository.unlock("lock"+url);
-        return "투표 성공";
-    }
 
-    public String likeCancel(String url, String storeId) {
-        //공유 데이터 찾기
-        Share share = (Share) redisRepository.getObject(url);
-        if(share == null)
-            throw new CustomException(ErrorCode.NOT_FOUND_URL);
-//        Share share = shareRepository.findById(Long.parseLong(shareId)).get();
-
+        //voteStoreList 데이터 변경
         HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
         Long key = Long.parseLong(storeId);
         voteStoreList.replace(key, voteStoreList.get(key)-1);
-//        shareRepository.save(share);
+
+        //redis에 저장
         redisRepository.setValuesObject(url, share, Duration.ofMinutes(5));
 
+        //TODO: 디버그 코드 추후 삭제
         System.out.println("[after cancel]");
         for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
             System.out.println("key : " + entry.getKey() + " value : " + entry.getValue());
         }
+
+        //redis unLock
+        redisLockRepository.unlock("likeCancel"+url+storeId);
         return "투표 취소";
     }
 
@@ -213,13 +216,15 @@ public class ShareService {
         Share share = (Share) redisRepository.getObject(url);
         if(share == null)
             throw new CustomException(ErrorCode.NOT_FOUND_URL);
-//        Share share = shareRepository.findById(Long.parseLong(shareId)).get();
 
-        HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
+        //최댓값 찾기
         int max = 0;
+        HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
         for (Integer value : voteStoreList.values()) {
             max = Math.max(max, value);
         }
+
+        //최댓값에 해당하는 데이터 찾기
         List<StoreResponse> ret = new ArrayList<>();
         for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
             if(entry.getValue() == max){
@@ -231,10 +236,12 @@ public class ShareService {
 
 
     public List<StoreSearchResponse> showVoteList(String url) {
+        //share entity 조회
         Share share = (Share) redisRepository.getObject(url);
         if(share == null)
             throw new CustomException(ErrorCode.NOT_FOUND_URL);
 
+        //voteStoreList 를 StoreSearchResponse 로 변환 후 반환
         HashMap<Long, Integer> voteStoreList = share.getVoteStoreList();
         List<StoreSearchResponse> ret = new ArrayList<>();
         for (Map.Entry<Long, Integer> entry : voteStoreList.entrySet()) {
