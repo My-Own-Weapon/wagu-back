519c42965418401a6dd85d62781ba871b355eb99
HyeokJoon <fufu98@naver.com>
2024-07-20 18:46:28 +0900

    [FEAT]/#114-LiveResponse 반환값 변경

--- src/main/java/com/chimaera/wagubook/dto/response/LiveResponse.java
+++ src/main/java/com/chimaera/wagubook/dto/response/LiveResponse.java
@@ -8,10 +8,15 @@ public class LiveResponse {
     String profileImage;    //라이브 중인 사람의 프로필 사진
     String sessionId;       //라이브 세션 ID
     String userName;        //라이브 중인 사람의 닉네임
+    String address;
+    String storeName;
+
 
     public LiveResponse(LiveRoom liveRoom){
         this.profileImage=liveRoom.getMember().getMemberImage().getUrl();
         this.sessionId=liveRoom.getSessionId();
         this.userName=liveRoom.getMember().getName();
+        this.storeName=liveRoom.getStore().getStoreName();
+        this.address=liveRoom.getStore().getStoreLocation().getAddress();
     }
 }
