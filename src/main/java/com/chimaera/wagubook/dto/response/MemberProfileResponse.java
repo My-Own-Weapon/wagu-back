b224abd8a3df10e330f3c6ccb9226d276e5c470c
HyeokJoon <fufu98@naver.com>
2024-07-21 20:42:19 +0900

    [CHORE]/-사용자 프로필 반환값에 이름 추가

--- src/main/java/com/chimaera/wagubook/dto/response/MemberProfileResponse.java
+++ src/main/java/com/chimaera/wagubook/dto/response/MemberProfileResponse.java
@@ -6,9 +6,11 @@ import lombok.Data;
 public class MemberProfileResponse {
     private String imageUrl; // 프로필 이미지 url
     private String username; // 사용자 id
+    private String name;     // 사용자 아룸
 
-    public MemberProfileResponse(String imageUrl, String username) {
+    public MemberProfileResponse(String imageUrl, String username, String name) {
         this.imageUrl = imageUrl;
         this.username = username;
+        this.name = name;
     }
 }
