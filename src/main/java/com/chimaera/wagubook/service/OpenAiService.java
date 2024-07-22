d85de02941804a304467ca5edb908b573d1e6cf2
수현 <su551732@gmail.com>
2024-07-22 16:50:56 +0900

    [FEAT]/#85-이미지 분석 외 텍스트만으로 생성할 수 있는 기능 추가

--- src/main/java/com/chimaera/wagubook/service/OpenAiService.java
+++ src/main/java/com/chimaera/wagubook/service/OpenAiService.java
@@ -24,6 +24,7 @@ public class OpenAiService {
 
     private final RestTemplate restTemplate;
 
+    // 이미지
     public String requestImageAnalysis(BufferedImage resizedImage, String menuName, String categoryName) throws IOException {
         // base 64 변환
         ByteArrayOutputStream os = new ByteArrayOutputStream();
@@ -35,13 +36,39 @@ public class OpenAiService {
 
         // 요청할 데이터 설정
         Map<String, Object> requestData = new HashMap<>();
-        requestData.put("model", "${openai.model}");
+        requestData.put("model", apiModel);
         requestData.put("max_tokens", 100);
         requestData.put("messages", Arrays.asList(
-                Map.of("role", "user", "content", String.format("주어진 음식 이미지를 분석해서 이에 대한 긍정적인 리뷰를 100글자 내외로 사용자가 자신의 기록을 남기는 어투로 작성해줘! " +
+                Map.of("role", "user", "content", String.format("주어진 음식 이미지를 분석해서 이에 대한 긍정적인 리뷰를 남겨줘" +
                         "해당 음식은 메뉴 이름이 %s이고, 음식 카테고리가 %s인 음식이야." +
-                        "이때 시간, 날짜 관련 표현은 제외해줘." +
-                        "출력은 리뷰만 해줘.", imageUrl, menuName, categoryName))
+                        "이때 숫자, 시간, 날짜 관련 표현은 제외해줘." +
+                        "또한 사용자가 기록을 남기는 어투로 작성해줘야 하고, 완성형 문장이어야 해!" +
+                        "마지막으로 출력은 리뷰만 해줘.", imageUrl, menuName, categoryName))
+        ));
+
+        // OpenAI API 호출
+        Map<String, Object> response = restTemplate.postForObject(apiUrl, requestData, Map.class);
+
+        List<Map<String, Object>> choices = (List<Map<String, Object>>) response.get("choices");
+        Map<String, Object> choice = choices.get(0);
+        Map<String, String> message = (Map<String, String>) choice.get("message");
+        String review = message.get("content");
+
+        return review;
+    }
+
+    // 텍스트
+    public String requestText(String menuName, String categoryName) throws IOException {
+        // 요청할 데이터 설정
+        Map<String, Object> requestData = new HashMap<>();
+        requestData.put("model", apiModel);
+        requestData.put("max_tokens", 100);
+        requestData.put("messages", Arrays.asList(
+                Map.of("role", "user", "content", String.format("주어진 음식 이미지를 분석해서 이에 대한 긍정적인 리뷰를 남겨줘" +
+                        "해당 음식은 메뉴 이름이 %s이고, 음식 카테고리가 %s인 음식이야." +
+                        "이때 숫자, 시간, 날짜 관련 표현은 제외해줘." +
+                        "또한 사용자가 기록을 남기는 어투로 작성해줘야 하고, 완성형 문장이어야 해!" +
+                        "마지막으로 출력은 리뷰만 해줘.", menuName, categoryName))
         ));
 
         // OpenAI API 호출
