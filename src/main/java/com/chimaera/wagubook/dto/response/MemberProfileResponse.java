09e320aae9364abea29106ef0988d5a7ebc8a9ff
jayjae97 <skyroof97@gmail.com>
2024-07-23 19:39:37 +0900

    [FIX]/#-memberId, imageUrl, username, name 반환하도록 수정

--- src/main/java/com/chimaera/wagubook/dto/response/MemberProfileResponse.java
+++ src/main/java/com/chimaera/wagubook/dto/response/MemberProfileResponse.java
@@ -4,11 +4,13 @@ import lombok.Data;
 
 @Data
 public class MemberProfileResponse {
+    private Long memberId;  // 사용자의 진짜 id
     private String imageUrl; // 프로필 이미지 url
-    private String username; // 사용자 id
+    private String username; // 사용자 별명
     private String name;     // 사용자 아룸
 
-    public MemberProfileResponse(String imageUrl, String username, String name) {
+    public MemberProfileResponse( Long memberId, String imageUrl, String username, String name) {
+        this.memberId = memberId;
         this.imageUrl = imageUrl;
         this.username = username;
         this.name = name;
