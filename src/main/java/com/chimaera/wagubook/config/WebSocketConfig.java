780d8ec3eedd0610d2221fa15b9e0b8f436561ff
HyeokJoon <fufu98@naver.com>
2024-08-08 19:33:05 +0900

    [FEAT]/#140-웹 소켓 config /chat 엔드포인트에서 ChatSignalHander 수행

--- src/main/java/com/chimaera/wagubook/config/WebSocketConfig.java
+++ src/main/java/com/chimaera/wagubook/config/WebSocketConfig.java
@@ -1,5 +1,5 @@
 package com.chimaera.wagubook.config;
-import com.chimaera.wagubook.controller.SignalingHandler;
+import com.chimaera.wagubook.controller.ChatSignalHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SignalingHandler(), "/signal")
+        registry.addHandler(new ChatSignalHandler(), "/chat")
                 .addInterceptors(new HttpSessionHandshakeInterceptor()) //웹 소켓 세션에서 httpSession정보를 가져오기 위해 HttpSession에 있던 정보들을 copy
                 .setAllowedOrigins("*");
     }
