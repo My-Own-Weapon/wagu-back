8b5d92715625c2992616758b6ca372cd20eafba6
HyeokJoon <fufu98@naver.com>
2024-08-08 19:32:20 +0900

    [FEAT]/#140-웹 소켓 채팅 메시지 request DTO

--- src/main/java/com/chimaera/wagubook/dto/request/WebSocketRequest.java
+++ src/main/java/com/chimaera/wagubook/dto/request/WebSocketRequest.java
@@ -5,13 +5,8 @@ import lombok.Data;
 @Data
 public class WebSocketRequest {
     private String senderId;
+    private String senderName;
     private String type;
     private String data;
     private String roomURL;
-    private String username;
-    private String receiver;
-    private Object offer;
-    private Object answer;
-    private Object candidate;
-    private Object sdp;
 }
